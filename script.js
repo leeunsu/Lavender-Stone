35b399bf73ebe58fbd40cee4dd95b2bb2e5689c2
“Eunsoo <leeunsu@live.comssh-keygen -t rsa -C leeunsu@live.comcat>
2020-02-17 12:04:40 -0800

    2.58

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ function openNav() {
 
 
 
-// $(document).scroll(function () {
-//     var y = $(this).scrollTop();
-//     if (y > 800) {
-//         $('.footer').fadeIn();
-//     } else {
-//         $('.footer').fadeOut();
-//     }
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 800) {
+        $('.footer').fadeIn();
+    } else {
+        $('.footer').fadeOut();
+    }
 
-// });
+});
 
 
 
